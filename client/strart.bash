9a317380282c05322f05266452adf598e8196f7b
Mario Barbosa <marioabarbosam@gmail.com>
2020-08-11 19:23:45 -0500

    bash para crear ambiente virtual

--- client/strart.bash
+++ client/strart.bash
@@ -3,9 +3,11 @@
 #for windows
 
 #python -m venv venv
-#source venv/Scripts/Activate 
+#source venv/Scripts/Activate
+#pip install -r requirements.txt
 
 #for linux
 
 #python3 -m venv venv
 #source venv/bin/activate
+#pip install -r requirements.txt
